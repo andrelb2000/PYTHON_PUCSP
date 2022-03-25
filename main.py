ba50f24a69b32837ad562509510b0962db35f37f
andrelb2000 <andre.luiz.braga@terra.com.br>
2022-03-25 08:36:23 -0300

    Autalizacao

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from CONTROLE import Controle
 from CONTROLE2 import Controle2
-controle = Controle()
-controle.executar()
+##controle = Controle()
+##controle.executar()
 
 controle2 = Controle2()
 controle2.executar()
