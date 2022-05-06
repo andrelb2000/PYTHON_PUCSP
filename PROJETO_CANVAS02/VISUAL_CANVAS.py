c43dac7097339c783d2a2b7b4435a6409fdb68fb
andrelb2000 <andre.luiz.braga@terra.com.br>
2022-05-06 07:59:12 -0300

    Controles de Multithread

--- PROJETO_CANVAS02/VISUAL_CANVAS.py
+++ PROJETO_CANVAS02/VISUAL_CANVAS.py
@@ -1,6 +1,40 @@
 
 from tkinter import Tk,Canvas,StringVar,Button,Label
-import time, threading
+import time, threading, random
+
+class DepositoTexto():
+
+    _listaPalavras_ = []
+    _semaforo_ = None
+    def __init__(self):
+        random.Random()
+        self._semaforo_ = threading.Semaphore()
+    def armazenaAleatoria(self):
+        self._semaforo_.acquire()
+        palavra = "Palavra_" + str(random.randint(0,10000))
+        self._listaPalavras_.append(palavra)
+        time.sleep(2)
+        self._semaforo_.release()
+    def armazena(self,palavra):
+        self._listaPalavras_.append(palavra)
+    def obterPalavraAleatoria(self):
+        self._semaforo_.acquire()
+        n = random.randint(0,len(self._listaPalavras_)-1)
+        time.sleep(2)
+        self._semaforo_.release()
+        return self._listaPalavras_[n]
+
+class DepositoTextoSimples(DepositoTexto):
+
+    def armazenaAleatoria(self):
+        palavra = "Palavra_" + str(random.randint(0,10000))
+        self._listaPalavras_.append(palavra)
+        time.sleep(2)
+
+    def obterPalavraAleatoria(self):
+        n = random.randint(0,len(self._listaPalavras_)-1)
+        time.sleep(2)
+        return self._listaPalavras_[n]
 
 
 class VisualCanvas():
@@ -8,25 +42,53 @@ class VisualCanvas():
     _status_ = None
     _buttonNovaJanela_ = None
     _buttonIniciaProcesso_ = None
+    _buttonIniciaProcesso2_ = None
+    _buttonIniciaProcesso3_ = None
+    _buttonIniciaProcesso4_ = None
+
     _logProcesso_ = None
     _textoLog_  = None
     _canvas_ = None
     _novaJanela_ = None
     _nrJanela_ = 0
     _linhaAtual_ = 0
-    def __init__(self,nr):
+    _deposito_ = None
+    _depositoLocal_ = None
+    _depositoSemControle_ = None
+    _depositoLocalSemControle_ = None
+
+    def __init__(self,nr,deposito,deposito2):
+        if deposito==None:
+            self._deposito_ = DepositoTexto()
+        else:
+            self._deposito_ = deposito
+
+        if deposito2==None:
+            self._depositoSemControle_ = DepositoTextoSimples()
+        else:
+            self._depositoSemControle_ = deposito2
+
+        self._depositoLocalSemControle_ = DepositoTexto()
         self._nrJanela_ = nr + 1
         self._janela_ = Tk()
         print("Aqui deveria criara outra variavel")
         self._status_ = Label(master=self._janela_)
         self._buttonNovaJanela_ = Button(master=self._janela_, text="Nova Janela", command=self.novaJanela)
         self._buttonIniciaProcesso_ = Button(master=self._janela_, text="Inicia Processo",  command=self.iniciaProcesso)
+        self._buttonIniciaProcesso2_ = Button(master=self._janela_, text="Inicia Processo - Deposito Local", command=self.iniciaProcesso2)
+        self._buttonIniciaProcesso3_ = Button(master=self._janela_, text="Inicia Processo - Sem Controle de Semaforo", command=self.iniciaProcesso3)
+        self._buttonIniciaProcesso4_ = Button(master=self._janela_, text="Inicia Processo - Deposito Local Sem Semaforo", command=self.iniciaProcesso4)
+
         self._textoLog_ = StringVar()
-        self._textoLog_.set("RELATORIO: \n")
+        self._textoLog_.set("RELATORIO - Janela: " + str(self._nrJanela_))
         self._logProcesso_   = Label(master=self._janela_, textvariable=self._textoLog_)
         self._canvas_ = Canvas(master=self._janela_, height=500, width=500, bg="yellow")
         self._buttonNovaJanela_.pack()
         self._buttonIniciaProcesso_.pack()
+        self._buttonIniciaProcesso2_.pack()
+        self._buttonIniciaProcesso3_.pack()
+        self._buttonIniciaProcesso4_.pack()
+
         self._status_.pack()
         self._logProcesso_.pack()
         self._canvas_.pack()
@@ -38,21 +100,104 @@ class VisualCanvas():
 
     def iniciaProcesso(self):
         inicio = time.perf_counter()
-        self.escreveLinha("Rodando... INICIO: " + str(round(inicio,2)))
-        t1 = threading.Thread(target=self.executarProcesso)
+        self.escreveLinha("Rodando... INICIO da chamada: " + str(round(inicio,2)))
+        t1 = threading.Thread(target=self.executarProcesso,args=[inicio])
         t1.start()
         fim = time.perf_counter()
-        self.escreveLinha("Rodando... FIM:    " + str(round(fim,2)))
-        self.escreveLinha("Tempo decorrido:  " + str(round(fim-inicio,2)))
+        self.escreveLinha("Rodando... Volta da Chamada: " + str(round(fim,2)))
+        self.escreveLinha("Tempo decorrido da chamada:  " + str(round(fim-inicio,2)))
+
+    def iniciaProcesso2(self):
+        inicio = time.perf_counter()
+        self.escreveLinha("Deposito Local... INICIO: " + str(round(inicio,2)))
+        t1 = threading.Thread(target=self.executarProcesso2,args=[inicio])
+        t1.start()
+        fim = time.perf_counter()
+        self.escreveLinha("Deposito Local... VOLTA: " + str(round(fim,2)))
+        self.escreveLinha("Tempo decorrido da chamada:  " + str(round(fim-inicio,2)))
+
+
+    def iniciaProcesso3(self):
+        inicio = time.perf_counter()
+        self.escreveLinha("Deposito S/Semaforo - INICIO: " + str(round(inicio,2)))
+        t1 = threading.Thread(target=self.executarProcesso3,args=[inicio])
+        t1.start()
+        fim = time.perf_counter()
+        self.escreveLinha("Deposito S/Semaforo - VOLTA: " + str(round(fim,2)))
+        self.escreveLinha("Tempo decorrido da chamada:  " + str(round(fim-inicio,2)))
+
+    def iniciaProcesso4(self):
+        inicio = time.perf_counter()
+        self.escreveLinha("Deposito Local S/Semaforo INICIO: " + str(round(inicio,2)))
+        t1 = threading.Thread(target=self.executarProcesso4,args=[inicio])
+        t1.start()
+        fim = time.perf_counter()
+        self.escreveLinha("Deposito Local S/Semaforo VOLTA: " + str(round(fim,2)))
+        self.escreveLinha("Tempo decorrido da chamada:  " + str(round(fim-inicio,2)))
+
+
 
     def novaJanela(self):
-        self._novaJanela_ = VisualCanvas(self._nrJanela_)
+        self._novaJanela_ = VisualCanvas(self._nrJanela_,self._deposito_,self._depositoSemControle_)
         self._novaJanela_.iniciar()
     def iniciar(self):
         self._janela_.mainloop()
 
-    def executarProcesso(self):
-        for i in range(10):
-            self.escreveLinha("Rodando... ")
-            time.sleep(1)
+    def processo(self):
+        self._deposito_.armazenaAleatoria()
+        palavra = self._deposito_.obterPalavraAleatoria()
+        self.escreveLinha("Obtendo Palavra: "+palavra)
 
+
+    def processo2(self):
+        self._depositoLocal_.armazenaAleatoria()
+        palavra = self._depositoLocal_.obterPalavraAleatoria()
+        self.escreveLinha("Obtendo Palavra: "+palavra)
+
+    def processo3(self):
+        self._depositoSemControle_.armazenaAleatoria()
+        palavra = self._depositoSemControle_.obterPalavraAleatoria()
+        self.escreveLinha("Obtendo Palavra: "+palavra)
+
+    def processo4(self):
+        self._depositoLocalSemControle_.armazenaAleatoria()
+        palavra = self._depositoLocalSemControle_.obterPalavraAleatoria()
+        self.escreveLinha("Obtendo Palavra: "+palavra)
+
+
+    def executarProcesso(self,disparado):
+
+        for i in range(10):
+            inicio = time.perf_counter()
+            time.sleep(1)
+            self.processo()
+            fim = time.perf_counter()
+            self.escreveLinha("Rodando "+str(i)+ " Tempo: " + str(round(fim - inicio, 1)))
+        self.escreveLinha("Tempo desde a chamada: " + str(round(fim - disparado, 3)))
+
+    def executarProcesso2(self,disparado):
+        for i in range(10):
+            inicio = time.perf_counter()
+            time.sleep(1)
+            self.processo2()
+            fim = time.perf_counter()
+            self.escreveLinha("Rodando "+str(i)+ " Tempo: " + str(round(fim - inicio, 1)))
+        self.escreveLinha("Tempo desde a chamada: " + str(round(fim - disparado, 3)))
+
+    def executarProcesso3(self,disparado):
+        for i in range(10):
+            inicio = time.perf_counter()
+            time.sleep(1)
+            self.processo3()
+            fim = time.perf_counter()
+            self.escreveLinha("Rodando "+str(i)+ " Tempo: " + str(round(fim - inicio, 1)))
+        self.escreveLinha("Tempo desde a chamada: " + str(round(fim - disparado, 3)))
+
+    def executarProcesso4(self, disparado):
+        for i in range(10):
+            inicio = time.perf_counter()
+            time.sleep(1)
+            self.processo4()
+            fim = time.perf_counter()
+            self.escreveLinha("Rodando " + str(i) + " Tempo: " + str(round(fim - inicio, 1)))
+        self.escreveLinha("Tempo desde a chamada: " + str(round(fim - disparado, 3)))
--- PROJETO_CANVAS02/main.py
+++ PROJETO_CANVAS02/main.py
@@ -1,6 +1,6 @@
 from VISUAL_CANVAS import *
 
-visual = VisualCanvas(0)
+visual = VisualCanvas(0,None,None)
 visual.iniciar()
 
 
